ca15c0a8ffb027aca64d9875086e9865f260cd5b
kpopovic89 <kacapopovic001@gmail.com>
2024-01-28 17:17:53 +0100

    omogucena login funkcionalnost

--- dmsreact/src/komponente/auth/Login.jsx
+++ dmsreact/src/komponente/auth/Login.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { FaUser } from "react-icons/fa";
-import InputField from './InputField'; 
+import InputField from './InputField';
+import axios from 'axios'; 
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: 'ana@gmail.com',
+    password: 'anaana1234',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/login', formData);  
+      const { token } = response.data;
+      
+      sessionStorage.setItem('token', token);  
+      console.log('Login successful');  
+    
+    } catch (error) {
+      console.error(error);
+     
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-card">
         <div className="login-icon">
           <FaUser />
         </div>
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
           <InputField
             label="Email Address"
             type="email"
             id="email"
             name="email"
-            placeholder="Username@gmail.com"
             required
+            value={formData.email}
+            onChange={handleChange}
           />
           <InputField
             label="Password"
@@ -25,6 +52,8 @@ const Login = () => {
             id="password"
             name="password"
             required
+            value={formData.password}
+            onChange={handleChange}
           />
           <button type="submit" className="login-button">Login</button>
         </form>
