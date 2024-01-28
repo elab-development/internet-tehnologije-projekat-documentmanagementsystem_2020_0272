e5f558eb0af84f64b8cc3ab3bba1be3427584a3b
Mateja444 <eauditore40000@gmail.com>
2024-01-28 17:00:38 +0100

    kreirane rute na frontu

--- dmsreact/src/App.js
+++ dmsreact/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Home from './komponente/pocetna/Home';
 import Login from './komponente/auth/Login';
 import Register from './komponente/auth/Register';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-      <Login></Login>
-      <Register></Register>
-    </div>
+
+
+      <Router>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+    </Router>
+    
+      
   );
 }
 
