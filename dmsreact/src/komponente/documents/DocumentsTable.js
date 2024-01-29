b2651c368afcd0be5c8703b9973f9a60fe5d8e78
Mateja444 <eauditore40000@gmail.com>
2024-01-29 22:45:26 +0100

    dodati filteri

--- dmsreact/src/komponente/documents/DocumentsTable.js
+++ dmsreact/src/komponente/documents/DocumentsTable.js
@@ -12,6 +12,9 @@ const DocumentsTable = () => {
     const { documents,setDocuments, error } = useDocuments();
     const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [currentDocument, setCurrentDocument] = useState(null);
+    const [filterByTags, setFilterByTags] = useState([]);
+    const [filterByCategory, setFilterByCategory] = useState('');
+    const [filteredDocuments, setFilteredDocuments] = useState([]);
     const {categories,setCategories} = useCategories();
     const {tags,setTags} = useTags();
     const handleEdit = (document) => {
@@ -93,11 +96,65 @@ const DocumentsTable = () => {
                 console.error('Došlo je do greške prilikom brisanja dokumenta:', error);
             });
     };
+    useEffect(() => {
+     
+        const filterDocuments = () => {
+            let updatedFilteredDocuments = [...documents];
+
+            if (filterByCategory) {
+                updatedFilteredDocuments = updatedFilteredDocuments.filter(document => document.category_id.toString() === filterByCategory);
+            }
+
+            if (filterByTags.length > 0) {
+                updatedFilteredDocuments = updatedFilteredDocuments.filter(document =>
+                    filterByTags.every(tag => document.tags.map(tag => tag.toString()).includes(tag))
+                );
+            }
+
+            setFilteredDocuments(updatedFilteredDocuments);
+        };
+
+        filterDocuments();
+    }, [documents, filterByTags, filterByCategory]);   // Ovde primenjujemo filtere svaki put kada se promeni stanje `documents`, `filterByTags`, ili `filterByCategory`
+      
     if (error) {
         return <div>Došlo je do greške: {error.message}</div>;
     }
     return (
         <div className="documents-container">
+            <div className="filter-container">
+                <div className="input-group">
+                    <label htmlFor="filter-category">Filter by Category</label>
+                    <select id="filter-category" value={filterByCategory} onChange={(e) => setFilterByCategory(e.target.value)}>
+                    <option value="">All Categories</option>
+                    {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                        {category.name}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="filter-tags">Filter by Tags</label>
+                    <select id="filter-tags" multiple value={filterByTags} onChange={(e) => {
+                    const options = e.target.options;
+                    const value = [];
+                    for (let i = 0, l = options.length; i < l; i++) {
+                        if (options[i].selected) {
+                        value.push(options[i].value);
+                        }
+                    }
+                    setFilterByTags(value);
+                    }}>
+                    {tags.map((tag) => (
+                        <option key={tag.id} value={tag.id}>
+                        {tag.name}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+                </div>
+
             <table className="documents-table">
                 <thead>
                     <tr>
@@ -114,16 +171,17 @@ const DocumentsTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {documents.map((document) => (
-                       <DocumentTableRow
-                            key={document.id}
-                            document={document}
-                            onDelete={handleDelete}
-                            onDownload={handleDownload}
-                            onEdit={handleEdit}
+                    {filteredDocuments.map((document) => (
+                        <DocumentTableRow
+                        key={document.id}
+                        document={document}
+                        onDelete={handleDelete}
+                        onDownload={handleDownload}
+                        onEdit={handleEdit}
                         />
                     ))}
-                </tbody>
+                    </tbody>
+
             </table>
             {isEditModalOpen && currentDocument && (
                 <DocumentEditModal
